6413719eded5abea0b61da5a6bae0d33a351a3bd
carrexxii <cjbarrett101@outlook.com>
2024-02-14 18:54:00 +0200

    Working vm frontend

--- src/csc/asa/asm.fs
+++ src/csc/asa/asm.fs
@@ -8,6 +8,11 @@ open ASAEmulator
 open ASAEmulator.Tokens
 
 module ASM =
+    type HistoryEntry =
+        { pc : int
+          acc: int
+          ix : int
+          mem: (int * int) option }
     [<ReactComponent>]
     let Emulator () =
         let loading, setLoading = React.useState true
@@ -26,76 +31,140 @@ module ASM =
                 setLoading false
             } |> Async.StartImmediate
         Html.div [
-            prop.className "flex flex-row"
+            prop.className "flex flex-col"
             prop.children [
                 match loading with
                 | true  -> Loading ()
                 | false ->
                     Button "Step" (fun e ->
-                        let memState = state.memory
-                                       |> Seq.sortBy _.Key
-                                       |> Seq.map (fun e -> e.Key, e.Value)
-                                       |> List.ofSeq
-                        setHistory (memState::history)
-                        setState (VM.step state))
-                    Html.ul (
-                        state.program.instrs
-                        |> Array.map (fun instr ->
-                            Html.li [
-                                prop.className "bg-green-300"
-                                prop.children [
-                                    Html.text $"{instr}"
-                                ]
-                            ])
-                    )
+                        if state.cir = END then ()
+                        else
+                            let newState = VM.step state
+                            let mem = match newState.cir with
+                                      | STO addr -> Some (addr, newState.acc)
+                                      | _ -> None
+                            let entry =
+                                { pc  = state.pc
+                                  acc = newState.acc
+                                  ix  = newState.ix
+                                  mem = mem }
+                            setHistory (entry::history)
+                            setState newState)
+
+                    let li (name: string) (value: string) =
+                        Html.li [
+                            prop.className "inline m-4 px-2 py-1"
+                            prop.children [
+                                Html.b name
+                                Html.text value
+                            ]
+                        ]
                     Html.ul [
-                        prop.className "text-justify bg-red-200"
-                        prop.children (
-                            [ $"{state.acc}"; $"{state.cir}"; $"{state.pc}" ]
-                            |> List.map (fun reg ->
-                                Html.li [
-                                    prop.className "inline"
-                                    prop.children [
-                                        Html.text $"{reg}"
-                                    ]
-                                ])
-                        )
-                    ]
-                    Html.table [
-                        prop.className "h-fit bg-slate-200"
+                        prop.className "m-4 grow text-center border-b-2"
                         prop.children [
-                            Html.thead [
-                                prop.className "font-semibold"
+                            li "PC: " $"{state.pc}"
+                            li "CIR: " $"{state.cir}"
+                            li "ACC: " $"{state.acc}"
+                            li "IX: " $"{state.ix}"
+                            li "MAR: " $"{state.mar}"
+                            li "MDR: " $"{state.mdr}"
+                            li "flag: " $"{state.flag}"
+                        ]
+                    ]
+
+                    Html.div [
+                        prop.className "flex flex-row"
+                        prop.children [
+                            Html.ul [
+                                prop.className "mx-2 text-nowrap"
                                 prop.children (
-                                    state.memory
-                                    |> Seq.sortBy _.Key
-                                    |> Seq.map (fun addr ->
-                                        Html.th [
-                                            prop.className "p-2"
+                                    state.program.instrs
+                                    |> Array.mapi (fun i instr ->
+                                        Html.li [
+                                            prop.className $"""p-1 rounded-md
+                                                            {if state.pc = i then "bg-green-200" else ""}"""
                                             prop.children [
-                                                Html.text $"{addr.Key}"
+                                                Html.b $"{i + state.program.start} "
+                                                Html.text $"{instr}"
                                             ]
                                         ])
                                 )
                             ]
-                            Html.tbody (
-                                history
-                                |> List.rev
-                                |> List.map (fun memState ->
-                                    Html.tr [
-                                        prop.className "h-8 align-top"
-                                        prop.children (
-                                            memState
-                                            |> List.map (fun mem ->
-                                                Html.td [
-                                                    prop.className "w-12 text-center border-2 border-black"
-                                                    prop.children [
-                                                        Html.text $"{snd mem}"
-                                                    ]
+                            Html.table [
+                                prop.className "h-fit m-4"
+                                prop.children [
+                                    Html.thead [
+                                        prop.className "font-semibold"
+                                        prop.children [
+                                            Html.tr [
+                                                Html.th [
+                                                    prop.colSpan 3
+                                                ]
+                                                Html.th [
+                                                    prop.className "border-2 text-center text-xl"
+                                                    prop.colSpan state.memory.Count
+                                                    prop.text "Memory Address"
+                                                ]
+                                            ]
+
+                                            let th (text: string) =
+                                                Html.th [
+                                                    prop.className "w-32 border-2 px-2 py-1"
+                                                    prop.text text
+                                                ]
+                                            Html.tr ([ th "Instruction"; th "ACC"; th "IX" ]
+                                            @(state.memory
+                                                |> Seq.sortBy _.Key
+                                                |> Seq.map (fun addr -> th $"{addr.Key}")
+                                                |> Seq.toList))
+                                        ]
+                                    ]
+
+                                    let td (inner: string) =
+                                        Html.td [
+                                            prop.className "border-2 text-center"
+                                            prop.text inner
+                                        ]
+                                    Html.tbody ([
+                                        Html.tr ([
+                                            td ""
+                                            td ""
+                                            td ""
+                                        ]@(state.program.memory
+                                            |> Array.rev
+                                            |> Array.map (fun mem -> td $"{snd mem}")
+                                            |> List.ofArray))
+                                    ]@(history
+                                        |> List.rev
+                                        |> List.mapi (fun i entry ->
+                                            if i = 0 then Html.none
+                                            else
+                                                Html.tr [
+                                                    prop.className "h-8 align-top"
+                                                    prop.children ([
+                                                            td $"{entry.pc + state.program.start}"
+
+                                                            // len - i due to the reversing
+                                                            if entry.acc <> history[history.Length - i].acc
+                                                            then td $"{entry.acc}"
+                                                            else td ""
+
+                                                            if entry.ix <> history[history.Length - i].ix
+                                                            then td $"{entry.ix}"
+                                                            else td ""
+                                                    ]@(state.memory.Keys
+                                                        |> Seq.rev
+                                                        |> Seq.map (fun mem ->
+                                                            td (match entry.mem with
+                                                                | Some (addr, value)
+                                                                    when addr = mem -> $"{value}"
+                                                                | _ -> ""))
+                                                        |> Seq.toList
+                                                    ))
                                                 ])
-                                        )
-                                    ])
-                            )
+                                    ))
+                                ]
+                            ]
                         ]
                     ]
             ]
